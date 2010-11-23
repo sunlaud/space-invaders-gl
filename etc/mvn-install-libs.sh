300a13fcd2f55ea159d52682b8ce32f5da9585bf
xdev <xaoc3dgraph@gmail.com>
2010-11-23 22:09:23 +0200

    Add linux amd64 and windows native libs

---
+++ etc/mvn-install-libs.sh
@@ -0,0 +1,4 @@
+mvn install:install-file -DgroupId=com.gagetimer -DartifactId=gagetimer -Dversion=1.0 -Dpackaging=jar -Dfile=lib/gagetimer.jar
+mvn install:install-file -DgroupId=net.java.dev -DartifactId=jogl -Dversion=1.1.1 -Dpackaging=jar -Dfile=lib/jogl.jar
+mvn install:install-file -DgroupId=net.java.dev -DartifactId=gluegen-rt -Dversion=1.1.1 -Dpackaging=jar -Dfile=lib/gluegen-rt.jar
+mvn install:install-file -DgroupId=net.java.dev -DartifactId=openmali -Dversion=1.0 -Dpackaging=jar -Dfile=lib/openmali.jar 
--- runGame.sh
+++ runGame.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-java -Djava.library.path=./etc/gl_native_libs/ -jar ./space-invaders-gl-game/target/space-invaders-gl-game-1.0-SNAPSHOT.jar
+ARCH=i586
+OS=linux
+NATIVES=./etc/native/$OS/$ARCH/
+JAR_FILE=./space-invaders-gl-game/target/space-invaders-gl-game-1.0-SNAPSHOT.jar
+
+java -Djava.library.path=$NATIVES -jar $JAR_FILE
