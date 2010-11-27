688e9602fdbfe444c7b54207f6e03c386381385f
Andrey Klochkov <andrey3diq@gmail.com>
2010-11-27 17:37:47 +0200

    Windows bat scripts changed, OGL dlls fixed, GAGE timer removed and replaced by Java native System.nanoTimer()

---
+++ etc/mvn_install_common_libs.bat
@@ -0,0 +1,3 @@
+mvn install:install-file -DgroupId=net.java.dev -DartifactId=jogl -Dversion=1.1.1 -Dpackaging=jar -Dfile=etc\lib\ogl.jar
+mvn install:install-file -DgroupId=net.java.dev -DartifactId=gluegen-rt -Dversion=1.1.1 -Dpackaging=jar -Dfile=etc\lib\gluegen-rt.jar
+mvn install:install-file -DgroupId=net.java.dev -DartifactId=openmali -Dversion=1.0 -Dpackaging=jar -Dfile=etc\lib\openmali.jar 
