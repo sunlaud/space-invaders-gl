4bbbc5bf5881aa260ce5d3eaf642dc3bccc86e04
xdev <xaoc3dgraph@gmail.com>
2010-11-28 10:47:52 +0200

    core refactoring

---
+++ space-invaders-gl-game/src/main/scala/com/xdev/si/gllisteners/GameInfoHolder.scala
@@ -0,0 +1,11 @@
+package com.xdev.si.gllisteners
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: xdev
+ * Date: Nov 28, 2010
+ * Time: 1:39:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+object GameInfoHolder
