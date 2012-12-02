2cede1d08b9b7a98cde1eb9d0ed0c363dd204fd9
Den <xdev.developer@gmail.com>
2012-12-03 01:38:54 +0200

    working with quadtree

--- sengine-2d/src/main/scala/com/xdev/engine/collisions/Quadtree.scala
+++ sengine-2d/src/main/scala/com/xdev/engine/collisions/Quadtree.scala
@@ -5,10 +5,66 @@ import org.openmali.vecmath2.Vector3f
 import org.openmali.spatial.{SpatialNode, AxisIndicator}
 import org.openmali.spatial.bodies.{Box, Classifier}
 import org.openmali.spatial.bodies.Classifier.Classification
+import collection.mutable.ArrayBuffer
 
-class Quadtree[T <: SpatialNode](center: Vector3f, w: Float, h: Float) extends QuadTree[T](center, AxisIndicator.POSITIVE_Z_AXIS, w, h, h, false) {
+class Quadtree[T <: SpatialNode](center: Vector3f, w: Float, h: Float) extends QuadTree[T](center, AxisIndicator.POSITIVE_Z_AXIS, w, h, false) {
 
+  setMinNodesBeforeSplit(4)
   def getClassifier(target: Box, node: QuadCell[T]): Classification  = {
     Classifier.classifyBoxBox(node, target)
+  }
+
+  def getIntersectNodes(target: Box): List[T] = {
+    val nodes = new ArrayBuffer[T]()
+    val root = getRootCell
+    findNodes(target, root, nodes)
+    nodes.toList
+  }
+
+  private def findNodes(target: Box, cell: QuadCell[T], nodesHolder: ArrayBuffer[T]){
+    val classifier = getClassifier(target, cell)
+   // println("%s[%s] -> %s[%s] - %s".format(target.getCenter(new Vector3f()), target.getSize, cell.getCenter(new Vector3f()), cell.getSize, classifier))
+    if (classifier == Classification.OUTSIDE){
+       return
+    }
+
+    for (i <- 0 until cell.getNumNodes) {
+      nodesHolder += cell.getNode(i)
+    }
+
+    if (cell.hasChildCells){
+      if (cell.getCellHBack != null){
+          findNodes(target, cell.getCellHBack, nodesHolder)
+      }
+      if (cell.getCellHFront != null){
+        findNodes(target, cell.getCellHFront, nodesHolder)
+
+      }
+      if (cell.getCellHLeft != null){
+        findNodes(target, cell.getCellHLeft, nodesHolder)
+
+      }
+      if (cell.getCellHRight != null){
+        findNodes(target, cell.getCellHRight, nodesHolder)
+
+      }
+      if (cell.getCellQuBackLeft != null){
+        findNodes(target, cell.getCellQuBackLeft, nodesHolder)
+
+      }
+      if (cell.getCellQuBackRight != null){
+        findNodes(target, cell.getCellQuBackRight, nodesHolder)
+
+      }
+      if (cell.getCellQuFrontLeft != null){
+        findNodes(target, cell.getCellQuFrontLeft, nodesHolder)
+
+      }
+      if (cell.getCellQuFrontRight != null){
+        findNodes(target, cell.getCellQuFrontRight, nodesHolder)
+
+      }
+
+    }
   }
 }
--- sengine-2d/src/test/scala/com/xdev/engine/test/QuadTreeTest.scala
+++ sengine-2d/src/test/scala/com/xdev/engine/test/QuadTreeTest.scala
@@ -5,11 +5,12 @@ import com.xdev.engine.collisions.{QuadTreeNode, QuadTreeBuilder}
 import org.openmali.vecmath2.{Point3f, Vector3f}
 import com.xdev.engine.util.BoundingBoxUtils
 import org.openmali.spatial.bodies.Classifier
+import collection.mutable.ArrayBuffer
 
 
 class QuadTreeTest extends SpecificationWithJUnit{
-   "Quad tree" should {
-     "point 10,10 must be inside of tree " in {
+   "Quad tree cx50 cy50 100x100" should {
+     "point cx10 cy10 must be inside of tree " in {
        val nodes: List[QuadTreeNode] = List(
          new QuadTreeNode(new Point3f(10, 10, 0), 10),
          new QuadTreeNode(new Point3f(20, 20, 0), 10),
@@ -21,7 +22,7 @@ class QuadTreeTest extends SpecificationWithJUnit{
        "INSIDE" must equalTo(tree.getClassifier(BoundingBoxUtils.createBox(shot.center, shot.size), root).toString)
      }
 
-     "point 120,120 must be outside of tree  " in {
+     "point cx120 cy120 must be outside of tree  " in {
        val nodes: List[QuadTreeNode] = List(
          new QuadTreeNode(new Point3f(10, 10, 0), 10),
          new QuadTreeNode(new Point3f(20, 20, 0), 10),
@@ -30,6 +31,21 @@ class QuadTreeTest extends SpecificationWithJUnit{
        val tree = new QuadTreeBuilder[QuadTreeNode]().build(nodes, new Vector3f(50,50,0), 100, 100)
        val root = tree.getRootCell()
        "OUTSIDE" must equalTo(tree.getClassifier(BoundingBoxUtils.createBox(new Point3f(120, 120, 0), 10), root).toString)
+     }
+
+     "5x20 nodes" in {
+       val nodes = new ArrayBuffer[QuadTreeNode]()
+       for(y <- 0 until 10) {
+         for(x <- 0 until 10) {
+           nodes += new QuadTreeNode(new Point3f((x * 15), (y * 15), 0.0f), 30)
+         }
+       }
+       val tree = new QuadTreeBuilder[QuadTreeNode]().build(nodes.toList, new Vector3f(100,100,0), 200, 200)
+       val root = tree.getRootCell()
+       val box = BoundingBoxUtils.createBox(new Point3f(15, 15, 0), 10)
+       "INSIDE" must equalTo(tree.getClassifier(box, root).toString)
+
+       println("Found nodes: " + tree.getIntersectNodes(BoundingBoxUtils.createBox(new Point3f(15, 15, 0), 20)).size)
      }
    }
 }
